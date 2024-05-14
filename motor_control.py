8cefdc9ede147bc1c375546795afbdfa96bdf2cc
AshwathAch <120765291+AshwathAch@users.noreply.github.com>
2024-05-14 10:41:14 +0530

    Update motor_control.py

--- motor_control.py
+++ motor_control.py
@@ -1,35 +1,52 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-# GPIO pins connected to motor driver
-forward_pin = 17
-reverse_pin = 18
-
-# Setup GPIO
+# Set GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(forward_pin, GPIO.OUT)
-GPIO.setup(reverse_pin, GPIO.OUT)
 
-def forward():
-    GPIO.output(forward_pin, GPIO.HIGH)
-    GPIO.output(reverse_pin, GPIO.LOW)
-    print("Motor moving forward")
-    # Add any other motor control logic here
-    sleep(5)  # Motor runs for 5 seconds
-    GPIO.output(forward_pin, GPIO.LOW)
+# Motor control pins
+IN1 = 17
+IN2 = 18
+IN3 = 27
+IN4 = 22
 
-def reverse():
-    GPIO.output(forward_pin, GPIO.LOW)
-    GPIO.output(reverse_pin, GPIO.HIGH)
-    print("Motor moving reverse")
-    # Add any other motor control logic here
-    sleep(5)  # Motor runs for 5 seconds
-    GPIO.output(reverse_pin, GPIO.LOW)
+# Set up GPIO pins
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
 
-# Clean up GPIO on exit
-def cleanup_gpio():
-    GPIO.cleanup()
+# Function to control motors
+def move_forward():
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
 
-# Call cleanup function on exit
-import atexit
-atexit.register(cleanup_gpio)
+def move_backward():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+
+def stop_motors():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+
+# Main program
+try:
+    while True:
+        move_forward()  # Move motors forward
+        time.sleep(2)   # Run motors for 2 seconds
+        stop_motors()   # Stop motors
+        time.sleep(1)   # Pause for 1 second
+        move_backward() # Move motors backward
+        time.sleep(2)   # Run motors backward for 2 seconds
+        stop_motors()   # Stop motors
+        time.sleep(1)   # Pause for 1 second
+
+except KeyboardInterrupt:
+    stop_motors()     # Stop motors on keyboard interrupt
+    GPIO.cleanup()    # Clean up GPIO on exit
